4df41663e43a4f25b9e7c64504580d9b373cf974
vannguyenv12 <npnv.vn1@gmail.com>
2024-08-12 11:22:28 +0700

    playground prisma

--- src/features/user/controllers/user.controller.ts
+++ src/features/user/controllers/user.controller.ts
@@ -1,10 +1,13 @@
 import { NextFunction, Request, Response } from 'express';
+import prisma from '~/prisma';
 
 class UserController {
-  public getAll(req: Request, res: Response, next: NextFunction) {
+  public async getAll(req: Request, res: Response, next: NextFunction) {
+    const users = await prisma.user.findMany();
+
     res.status(200).json({
       message: 'Get all users successfully',
-      data: []
+      data: users
     });
   }
 }
